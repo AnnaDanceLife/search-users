4ea491416ba1742bf676662077400663fc40825f
Anna Shatilova <anxet@mail.ru>
2024-02-02 16:05:47 +0300

    refactoring and improving code performance

--- src/pages/main-page/MainPage.jsx
+++ src/pages/main-page/MainPage.jsx
@@ -1,21 +1,61 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import * as S from './MainPage.styles'
 import { Users } from '../../components/users/Users'
 import { Filter } from '../../components/filter/Filter'
 import { Loader, Error } from '../../App.styles'
 import { Pagination } from '../../components/pagination/Pagination'
+import { USER_PER_PAGE } from '../../utils/Constant'
+import { getUsers } from '../../api/apiUsers'
 
 export const MainPage = ({
+  usersList,
+  setUsersList,
   searchText,
   setSearchText,
-  usersList,
-  error,
-  isLoading,
-  pagesCount,
-  setOrder,
-  pageNumber,
   setPageNumber,
+  pageNumber,
 }) => {
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [pagesCount, setPagesCount] = useState(0)
+  const [order, setOrder] = useState('desc')
+
+  const getDataUsers = async () => {
+    await getUsers({ searchText, pageNumber, order })
+      .then((response) => {
+        setUsersList(response.data.items)
+        setPagesCount(Math.ceil(response.data.total_count / USER_PER_PAGE))
+        // setIsLoading(false)
+        if (response.data.items.length === 0) {
+          setError(
+            'Пользователи с таким логином не найдены. Проверьте корректность своего запроса.',
+          )
+          return
+        }
+      })
+      .catch((er) => {
+        // setIsLoading(false)
+        console.log(er)
+        if (er.response.status === 422) {
+          setError('Введите логин пользователя, которого хотите найти')
+        } else if (er.response.status === 403) {
+          setError('Превышен лимит запросов к серверу, попробуйте позже')
+        } else if (er.response.status === 503) {
+          setError('Сервис не доступен, попробуйте позже')
+        }
+        setError(er.message)
+      })
+      .finally(() => setIsLoading(false))
+  }
+
+  useEffect(() => {
+    if (searchText) {
+      setIsLoading(true)
+      setError(null)
+      getDataUsers()
+    }
+  }, [searchText, pageNumber, order])
+
   const handleNextPageClick = useCallback(() => {
     const current = pageNumber
     const next = current + 1
@@ -36,7 +76,10 @@ export const MainPage = ({
       <S.Search
         type="search"
         value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
+        onChange={(e) => {
+          setSearchText(e.target.value)
+          setPageNumber(1)
+        }}
         placeholder="Введите логин пользователя"
         name="search"
       />
@@ -45,6 +88,10 @@ export const MainPage = ({
         <Loader />
       ) : error ? (
         <Error>{error}</Error>
+      ) : !usersList.length > 0 ? (
+        <S.MainHeading>
+          Здесь будет список найденных пользователей
+        </S.MainHeading>
       ) : (
         <>
           <Filter setOrder={setOrder} />
